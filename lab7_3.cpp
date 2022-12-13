2b13b645f007d000ca404eed9bf2f54c5f6cfae8
isusboss <theeraphan_p@cmu.ac.th>
2022-12-13 16:34:08 +0700

    7.3 completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,14 +2,28 @@
 
 using namespace std;
 
+int adiff(int A,int B){
+  int C;
+  if(A>B){
+    C=(A-B)%360;
+  }
+  else{
+    C=(B-A)%360;
+  }
+  if(C >180){
+ C =360 -C;
+  }
+
+  return C;
+}
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
+  cout << adiff(180,270)<< "\n";
+  cout << adiff(210,45)<< "\n";
+  cout << adiff(0,360)<< "\n";
+  cout << adiff(10,350)<< "\n";
+  cout << adiff(95,260)<< "\n";
+  cout << adiff(90,-90)<< "\n";
+  cout << adiff(1000,280)<< "\n";
   cout << adiff(60,244);
 }
